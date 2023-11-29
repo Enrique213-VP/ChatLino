241654a4dc77f798284ae2b747c6f1bd91284ebd
Enrique213-VP <colombia00028@gmail.com>
2023-11-29 14:58:40 -0500

    Add register
    Create database and register with email using firebase

--- app/src/main/java/com/svape/chathappy/LoginActivity.kt
+++ app/src/main/java/com/svape/chathappy/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.svape.chathappy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.svape.chathappy.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signupText.setOnClickListener {
+            val intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+            onPause()
+        }
     }
 }
--- app/src/main/java/com/svape/chathappy/RegisterActivity.kt
+++ app/src/main/java/com/svape/chathappy/RegisterActivity.kt
@@ -1,11 +1,110 @@
 package com.svape.chathappy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.svape.chathappy.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var reference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar!!.hide()
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnRegister.setOnClickListener {
+            validateData()
+        }
+
+    }
+
+    private fun validateData() {
+
+        binding.apply {
+            val userName = username.text.toString()
+            val email = email.text.toString()
+            val password = password.text.toString()
+            val confirmPassword = confirmPassword.text.toString()
+
+            val fields = listOf(
+                Pair(userName, "Name is necessary"),
+                Pair(email, "Email is necessary"),
+                Pair(password, "Password is necessary"),
+                Pair(confirmPassword, "Validate password is necessary")
+            )
+
+            var allFieldsValid = true
+
+            for ((field, message) in fields) {
+                if (field.isEmpty()) {
+                    Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+                    allFieldsValid = false
+                    break
+                }
+            }
+
+            if (allFieldsValid) {
+                if (confirmPassword != password) {
+                    Toast.makeText(applicationContext, "Passwords do not match", Toast.LENGTH_LONG)
+                        .show()
+                } else {
+                    registerUser(email, password)
+                }
+            }
+        }
+
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                var uid = ""
+                uid = auth.currentUser!!.uid
+                reference = FirebaseDatabase.getInstance().reference.child("Users").child(uid)
+
+                val hashMap = HashMap<String, Any>()
+                val hashMapUserName = binding.username.text.toString()
+                val hashMapEmail = binding.email.text.toString()
+
+                hashMap["uid"] = uid
+                hashMap["nameUser"] = hashMapUserName
+                hashMap["email"] = hashMapEmail
+                hashMap["image"] = ""
+                hashMap["search"] = hashMapUserName.lowercase()
+
+                reference.updateChildren(hashMap).addOnCompleteListener { register ->
+                    if (register.isSuccessful) {
+                        val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                        Toast.makeText(
+                            applicationContext,
+                            "¡Bienvenido al combo!",
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
+                        startActivity(intent)
+                        finish()
+                    }
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(applicationContext, "${exception.message}", Toast.LENGTH_LONG)
+                        .show()
+                }
+            } else {
+                Toast.makeText(applicationContext, "Error en el registro", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+        }.addOnFailureListener { exception ->
+            Toast.makeText(applicationContext, "${exception.message}", Toast.LENGTH_LONG)
+                .show()
+        }
     }
 }

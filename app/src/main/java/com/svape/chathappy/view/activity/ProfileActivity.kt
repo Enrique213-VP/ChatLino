1815fcad2f7ad233ea12f01b24b3304d430c9acc
Enrique213-VP <colombia00028@gmail.com>
2024-03-03 17:51:10 -0500

    update gradle

--- app/src/main/java/com/svape/chathappy/view/activity/ProfileActivity.kt
+++ app/src/main/java/com/svape/chathappy/view/activity/ProfileActivity.kt
@@ -3,10 +3,14 @@ package com.svape.chathappy.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.svape.chathappy.R
+import com.svape.chathappy.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
